2eaa541cbc9049a0a1377c267dd59a8386ba6450
Juancarranza02 <79293560+Juancarranza02@users.noreply.github.com>
2023-05-29 22:10:53 -0600

    Primer Commit
    Primer Commit

---
+++ Preguntas_Prácticas_del_examen/src/models/AlumnoModels.java
@@ -0,0 +1,8 @@
+package models;
+
+public class AlumnoModels {
+    
+    public static void author63841(){
+        System.out.println("Nombre: Juan armando Carranza magadan\n Matricula: 63841 \n Correo Electronico: al063841@uacam.mx");
+    }
+}
